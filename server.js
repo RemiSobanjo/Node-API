bf6f01954d575bc365fb64137b88ab40e8236eaa
RemiSobanjo <53947149+RemiSobanjo@users.noreply.github.com>
2023-11-16 23:18:02 +0100

    Database connection created using mongoDB

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 //routes
@@ -6,7 +7,13 @@ app.get('/', (req, res) => {
     res.send('Hello NODE API');
 });
 
-
-app.listen(3000, () => {
-    console.log('Node API is running on port 3000');
-});
+mongoose.
+connect('mongodb+srv://admin:JhmLOqC8iVqxmtfb@fsaapi.zucf0dn.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+    console.log("DB Connection Successful");
+    app.listen(3000, () => {
+        console.log('Node API is running on port 3000');
+    });
+}).catch((error) => {
+    console.log(error);
+})

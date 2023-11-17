42d9656eeab83573ae205e0cad43742ca6daef9f
RemiSobanjo <53947149+RemiSobanjo@users.noreply.github.com>
2023-11-17 22:39:18 +0100

    delete product from db with id sent by user

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const Product = require('./models/productModel.js');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended : false}));
 
 //routes
 app.get('/', (req, res) => {
@@ -52,9 +53,24 @@ app.put('/products/:id', async(req, res) => {
         if(!product){
             return res.status(404).json({message: `cannot find any product with ID ${id}`});
         }
-        res.status(200).json(product);
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
 
     }catch( error ){
+        res.status(500).json({message: error.message});
+    }
+});
+
+//delete a product
+app.delete('/products/:id', async(req, res) => {
+    try{
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: `cannot find any products with ID ${id}`});
+        }
+        res.status(200).json(products);
+    } catch( error ){
         res.status(500).json({message: error.message});
     }
 });

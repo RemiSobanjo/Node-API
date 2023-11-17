4879b22c0693549a2fb97ad7f9951b4fcf0fe5ec
RemiSobanjo <53947149+RemiSobanjo@users.noreply.github.com>
2023-11-17 22:48:52 +0100

    Change feedback sent to user after product delete

--- server.js
+++ server.js
@@ -67,11 +67,11 @@ app.delete('/products/:id', async(req, res) => {
         const {id} = req.params;
         const product = await Product.findByIdAndDelete(id);
         if(!product){
-            return res.status(404).json({message: `cannot find any products with ID ${id}`});
+            return res.status(404).json({message: `cannot find any product with ID ${id}`});
         }
-        res.status(200).json(products);
+        res.status(200).json({message: `product with ID : ${id} has been deleted successfully `});
     } catch( error ){
-        res.status(500).json({message: error.message});
+        res.status(500).json({messsdsdsage: error.message});
     }
 });
 

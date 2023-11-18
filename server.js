aec15131d3515c5e67303d5b42c2252d7fa61c55
RemiSobanjo <53947149+RemiSobanjo@users.noreply.github.com>
2023-11-18 09:47:02 +0100

    enable cors for frontend to backend communication to a single site

--- server.js
+++ server.js
@@ -4,12 +4,20 @@ const mongoose = require('mongoose');
 const productRoute = require('./routes/productRoute.js');
 //const userRoute = require('./routes/userRoute.js');
 const errorMiddleware = require('./middleware/errorMiddleware');
+const cors = require('cors')
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_URL;
+const FRONTEND = process.env.FRONTEND;
 
+var corsOptions = {
+    origin: FRONTEND,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 
